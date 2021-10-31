c142ad2daaff3e736f2e4fa0b75e0364d970e160
rumansadiq <rumansadiq123@gmail.com>
2021-10-31 19:50:17 +0530

    first_commit

---
+++ myscript.js
@@ -0,0 +1,25 @@
+//to calculate the percentage//
+function percentage(num, per)
+{
+  return (num/100)*per;
+}
+          
+console.log(percentage(1000, 47.12));
+
+// program to check if the number is even or odd//
+const number = prompt("Enter a number: ");
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
+//reverse a string//
+function reverseString(str) {
+   
+    console.log(text.split("").reverse().join(""));
+}
+
+reverseString(" hello Ruman");
+
+
